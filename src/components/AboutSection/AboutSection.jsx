1da44289b6e971443a86a3092bc23df4aab91581
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-24 20:28:02 +0530

    React portfolio

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,19 @@
+import './AboutSection.css';
+
+function AboutSection() {
+  return (
+    <section id='about' className='about-section'>
+      <div className='about-wrapper container'>
+        <div className='about-image'>
+          <img src='\public\Mypic.jpeg' alt='Sanvi' />
+        </div>
+        <div className='about-copy'>
+          <h1>About Me</h1>
+          <p>Hi! I'm Sanvi, a CSE student and aspiring web developer, passionate about learning new technologies and building creative projects.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+export const categories = ['all', 'web'];
+
+export const projectsData = [
+  {
+    id: 1,
+    title: 'Amazon Clone',
+    description: 'A responsive e-commerce website inspired by Amazon.',
+    longDesc:
+      'A frontend Amazon clone created using HTML and CSS, focusing on layout, styling, navigation and responsive design.',
+    tech: ['HTML', 'CSS'],
+    category: 'web',
+    featured: true,
+    liveUrl: '#',
+    githubUrl: 'https://github.com/Sanvi-R-Rao/amazon-clone',
+  },
+];
